95855f39f29e6a6681377a56b0bd4a62410ab290
zizpzn <zizpzn@gmail.com>
2022-07-13 16:14:43 +0700

    request

--- 4_express/app.js
+++ 4_express/app.js
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
-  console.log("get");
+  console.log("path: ", req.path);
+  console.log("headers: ", req.headers);
+  console.log("params: ", req.params);
+  console.log("query: ", req.query);
 });
 
 app.listen(8080, () => {
